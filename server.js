ecba1b7f6ed1e12c7ff97fd7ba1c7c2e090d5d2a
‘Justin <jsrdeluna@gmail.com>
2015-05-09 12:11:02 -0700

    Bug fixes & add package.json

--- server.js
+++ server.js
@@ -55,6 +55,6 @@ io.on('connection', function(socket) {
 });
 
 http.listen(app.get('port'), function() {
-  console.log('Node app is running at localhost: ' + app.get('port'));
+  console.log('Node app is running at localhost: ' + app.get('port') + '!');
 });
 
